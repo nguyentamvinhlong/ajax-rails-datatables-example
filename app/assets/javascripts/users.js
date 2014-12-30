9e686852fef03c4fee0006ad812e4fa6b47d39cb
Tam Nguyen Thanh <tamnt@zigexn.vn>
2014-12-30 15:31:41 +0700

    update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     "serverSide": true,
     "ajax": $('#users-table').data('source'),
     "pagingType": "full_numbers",
+    "iDisplayLength": 25
     // optional, if you want full pagination controls.
     // Check dataTables documentation to learn more about
     // available options.
